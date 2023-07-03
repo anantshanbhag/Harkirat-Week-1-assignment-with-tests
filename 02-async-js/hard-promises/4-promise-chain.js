0dd27168e1c560c22f68a4575176712556559984
Anant Shanbhag <anant.s888@gmail.com>
2023-07-03 09:57:32 +0000

    Update 17 files

---
+++ 02-async-js/hard-promises/4-promise-chain.js
@@ -0,0 +1,42 @@
+/*
+ * Write 3 different functions that return promises that resolve after 1, 2, and 3 seconds respectively.
+ * Write a function that sequentially calls all 3 of these functions in order.
+ * Print out the time it takes to complete the entire operation.
+ * Compare it with the results from 3-promise-all.js
+ */
+
+function waitOneSecond() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 1000);
+  });
+}
+
+function waitTwoSecond() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 2000);
+  });
+}
+
+function waitThreeSecond() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 3000);
+  });
+}
+
+async function calculateTime() {
+  console.log(`Start: ${new Date()}`);
+
+  await waitOneSecond();
+  await waitTwoSecond();
+  await waitThreeSecond();
+
+  console.log(`End: ${new Date()}`);
+}
+
+calculateTime();
